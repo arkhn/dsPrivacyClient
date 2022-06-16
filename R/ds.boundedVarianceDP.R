c8bf539851501dfa45623feb6b33bb2f7b027d81
Théo Ryffel <theo.leffyr@gmail.com>
2022-06-16 14:07:19 +0200

    feat: add variance

---
+++ R/ds.boundedVarianceDP.R
@@ -0,0 +1,21 @@
+#' Title
+#'
+#' @param object
+#' @param option
+#' @param datasources
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ds.boundedVarianceDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound) {
+
+  if (is.null(datasources)) {
+    datasources <- DSI::datashield.connections_find()
+  }
+
+  cally <- paste0("boundedVarianceDP(", input_data, ", ", epsilon, ", ", lower_bound, ", ", upper_bound, ")")
+  result <- DSI::datashield.aggregate(datasources, as.symbol(cally))
+
+  return(result)
+}

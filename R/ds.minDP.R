fd77f5c8d61cfefc2bf75f500e03a6752a8b4389
Jasopaum <jason.paumier@gmail.com>
2022-06-30 17:53:09 +0200

    feat: add combine for min

--- R/ds.minDP.R
+++ R/ds.minDP.R
@@ -8,14 +8,17 @@
 #' @export
 #'
 #' @examples
-ds.minDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound) {
+ds.minDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound, type="combine") {
 
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
 
   cally <- paste0("minDP(", input_data, ", ", epsilon, ", ", lower_bound, ", ", upper_bound, ")")
-  result <- DSI::datashield.aggregate(datasources, as.symbol(cally))
+  res <- DSI::datashield.aggregate(datasources, as.symbol(cally))
 
-  return(result)
-}
+  combined <- min(unlist(res))
+
+  if (type=="combine") return(list(Min.by.Study=res))
+  if (type=="split") return(list(Global.Min=combined))
+  if (type=="both") return(list(Min.by.Study=res,Global.Min=combined))}

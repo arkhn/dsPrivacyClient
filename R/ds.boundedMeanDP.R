e9d9b16278f20239059bf57993047fd3a92ea420
Jasopaum <jason.paumier@gmail.com>
2022-06-30 17:46:24 +0200

    feat: add combine for mean

--- R/ds.boundedMeanDP.R
+++ R/ds.boundedMeanDP.R
@@ -8,14 +8,23 @@
 #' @export
 #'
 #' @examples
-ds.boundedMeanDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound) {
+
+
+ds.boundedMeanDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound, type="combine") {
 
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
 
   cally <- paste0("boundedMeanDP(", input_data, ", ", epsilon, ", ", lower_bound, ", ", upper_bound, ")")
-  result <- DSI::datashield.aggregate(datasources, as.symbol(cally))
+  ss.obj <- DSI::datashield.aggregate(datasources, as.symbol(cally))
 
-  return(result)
+  Nstudies <- length(datasources)
+  ss.mat <- matrix(as.numeric(matrix(unlist(ss.obj),nrow=Nstudies,byrow=TRUE)[,1:2]),nrow=Nstudies)
+
+  ss.mat.combined <- (t(matrix(ss.mat[,2]))%*%ss.mat[,1])/sum(ss.mat[,2])
+
+  if (type=="combine") return(list(Mean.by.Study=ss.mat[,1],Nstudies=Nstudies))
+  if (type=="split") return(list(Global.Mean=ss.mat.combined,Nstudies=Nstudies))
+  if (type=="both") return(list(Mean.by.Study=ss.mat[,1],Global.Mean=ss.mat.combined,Nstudies=Nstudies))
 }

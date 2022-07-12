be8818c80442996ec5d9df999be1615cfde92c2f
Théo Ryffel <theo.leffyr@gmail.com>
2022-07-12 16:06:40 +0200

    fix: bounded sum delta parameter

--- R/ds.boundedSumDP.R
+++ R/ds.boundedSumDP.R
@@ -8,13 +8,13 @@
 #' @export
 #'
 #' @examples
-ds.boundedSumDP <- function(datasources, input_data, epsilon, delta, lower_bound, upper_bound) {
+ds.boundedSumDP <- function(datasources, input_data, epsilon, lower_bound, upper_bound) {
 
   if (is.null(datasources)) {
     datasources <- DSI::datashield.connections_find()
   }
 
-  cally <- paste0("boundedSumDP(", input_data, ", ", epsilon, ", ", delta, ", ", lower_bound, ", ", upper_bound, ")")
+  cally <- paste0("boundedSumDP(", input_data, ", ", epsilon, ", ", lower_bound, ", ", upper_bound, ")")
   result <- DSI::datashield.aggregate(datasources, as.symbol(cally))
 
   return(result)

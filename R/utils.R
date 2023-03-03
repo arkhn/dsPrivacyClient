11e5561bad1a6359d981f14c4a9452f13c5b467b
Jasopaum <jason.paumier@gmail.com>
2023-03-03 16:28:15 +0100

    dev

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,15 @@
-#' @title Send a call to function in the server and get the result
-#'
-#' @param datasources a list of \code{\link{DSConnection-class}} 
-#' objects obtained after login.
-#' @param function_call a character string that represents the call to a function with its arguments.
-#'
-#' @return \code{callAggregationMethod} returns the result from a call to a function in the server
+callAggregationMethod <- function(datasources, function_call) {
+  #' @title Send a call to function in the server and get the result
+  #'
+  #' @param datasources a list of \code{\link{DSConnection-class}} 
+  #' objects obtained after login.
+  #' @param function_call a character string that represents the call to a function with its arguments.
+  #'
+  #' @return \code{callAggregationMethod} returns the result from a call to a function in the server
+  res <- DSI::datashield.aggregate(datasources, as.symbol(function_call))
+  return (res)
+} 
 
 # Small value to avoid misleading variance value (null or negative) due to differential privacy 
 # noise
 delta = 0.0001
-
-callAggregationMethod <- function(datasources, function_call) {
-  res <- DSI::datashield.aggregate(datasources, as.symbol(function_call))
-  return (res)
-} 
